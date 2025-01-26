86fb24fb7204a83250a5c752563276869aa5c41c
Germán David <manchov12@gmail.com>
2025-01-26 14:53:52 -0500

    installing sequelize

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express"
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import db from "./config/db.js"
 
 const app = express()
 
+try {
+    await db.authenticate()
+    console.log('Conexión existosa a la base de datos')
+} catch (error) {
+    console.log(error);
+}
 
 app.set('view engine', 'pug')
 app.set('views', './views')
