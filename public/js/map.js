302315dc06565d05596aefaeab9997aee2190f45
Germán David <manchov12@gmail.com>
2025-12-05 10:41:01 -0500

    instaling concurrently

--- public/js/map.js
+++ public/js/map.js
@@ -16,7 +16,7 @@
   \***********************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("{__webpack_require__.r(__webpack_exports__);\nalert('Soy el mapa y algo mas')\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/map.js?\n}");
+eval("{__webpack_require__.r(__webpack_exports__);\n\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/map.js?\n}");
 
 /***/ })
 
