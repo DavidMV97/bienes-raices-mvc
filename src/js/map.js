6771cca207d069064b0abb3f4abd5f5dc4909d2f
Germán David <manchov12@gmail.com>
2025-12-05 11:01:08 -0500

    Set location

---
+++ src/js/map.js
@@ -0,0 +1,11 @@
+(function () {
+    const lat = 3.4601173;
+    const lng = -76.5214537;
+    const mapa = L.map('map').setView([lat, lng], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
